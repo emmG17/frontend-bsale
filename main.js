35a78dc79c0e7dfefbabe4d20c2ed299b6085395
Emmanuel Gonzalez <emm.gs17@gmail.com>
2022-10-14 01:10:03 -0500

    Handle user input and search for products in db

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
+import { format } from "prettier";
 import createCategory from "./Category";
 import createProduct from "./Product";
 
-const API_URL = "http://reto-bsale-backend.herokuapp.com/";
+const API_URL = "http://localhost:3000/"; //"http://reto-bsale-backend.herokuapp.com/";
 
 // Request all categories
 async function getCategories() {
@@ -23,6 +24,12 @@ async function getProductsByCategory(id) {
   return products.products;
 }
 
+async function getProductsByQuery(product) {
+  let res = await fetch(`${API_URL}products/${product}`);
+  let products = await res.json();
+  return products.products;
+}
+
 async function refreshProductsByCategory(e) {
   const id = e.target.dataset.categoryId;
   const productList = document.getElementById("products");
@@ -30,6 +37,10 @@ async function refreshProductsByCategory(e) {
   let products =
     id === "-1" ? await getProducts() : await getProductsByCategory(id);
   populateProducts(productList, products);
+}
+
+async function refreshProductsFromSearch(e) {
+  e.preventDefault();
 }
 
 function populateProducts(parent, products) {
@@ -58,3 +69,18 @@ categories.map((li) => {
 // Place all products inside the product grid
 const productList = document.getElementById("products");
 populateProducts(productList, products);
+
+const searchForm = document.getElementById("search-form");
+searchForm.addEventListener("submit", async (e) => {
+  e.preventDefault();
+  // Get the user query
+  const formData = new FormData(e.target);
+  const productQuery = formData.get("productQuery");
+
+  // Search query in DB
+  const products = await getProductsByQuery(productQuery);
+
+  // Populate product grid
+  productList.textContent = "";
+  populateProducts(productList, products);
+});
