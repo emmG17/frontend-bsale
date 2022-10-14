f62cf2c25515b03790b8d527a68d0cf26811a579
Emmanuel Gonzalez <emm.gs17@gmail.com>
2022-10-14 00:34:02 -0500

    Create a function to populate any element with products. Refactor the category selection function

--- main.js
+++ main.js
@@ -6,27 +6,55 @@ const API_URL = "http://reto-bsale-backend.herokuapp.com/";
 // Request all categories
 async function getCategories() {
   let res = await fetch(API_URL + "categories");
-  return res.json();
+  let categories = await res.json();
+  return categories.categories;
 }
 
 // Request all products
 async function getProducts() {
   let res = await fetch(API_URL + "products");
-  return res.json();
+  let products = await res.json();
+  return products.products;
 }
 
-let categoriesJSON = await getCategories();
-let categories = categoriesJSON.categories;
+async function getProductsByCategory(id) {
+  let res = await fetch(`${API_URL}products/category/${id}`);
+  let products = await res.json();
+  return products.products;
+}
 
-const productsJSON = await getProducts();
-const products = productsJSON.products;
+async function refreshProductsByCategory(e) {
+  const id = e.target.dataset.categoryId;
+  const productList = document.getElementById("products");
+  productList.textContent = "";
+  let products =
+    id === "-1" ? await getProducts() : await getProductsByCategory(id);
+  populateProducts(productList, products);
+}
+
+function populateProducts(parent, products) {
+  products.map((p) => {
+    const product = createProduct(p);
+    parent.appendChild(product);
+  });
+}
+
+const categories = await getCategories();
+const products = await getProducts();
 
 // Put all categories inside the sidebar
 const categoryList = document.getElementById("category-list");
-categories.map((li) => categoryList.appendChild(createCategory(li)));
+// Create a category for ALL products
+const allProducts = createCategory({ id: -1, name: "todos" });
+allProducts.addEventListener("click", refreshProductsByCategory);
+categoryList.appendChild(allProducts);
+
+categories.map((li) => {
+  const category = createCategory(li);
+  category.addEventListener("click", refreshProductsByCategory);
+  categoryList.appendChild(category);
+});
 
 // Place all products inside the product grid
 const productList = document.getElementById("products");
-products.map((p) => {
-  productList.appendChild(createProduct(p));
-});
+populateProducts(productList, products);
