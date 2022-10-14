de1cf2fde2f26c77171a089c00a9088cdc3df8f4
Emmanuel Gonzalez <emm.gs17@gmail.com>
2022-10-13 23:38:09 -0500

    Transfer data formatting for categories outside the main file

--- Category.js
+++ Category.js
@@ -1,6 +1,8 @@
+import TitleCase from "./TitleCase";
+
 const createCategory = (category) => {
   const categoryItem = document.createElement("button");
-  categoryItem.innerText = category.name;
+  categoryItem.innerText = TitleCase(category.name);
   categoryItem.classList.add("list-group-item", "list-group-item-action");
   categoryItem.dataset.categoryId = category.id;
   return categoryItem;
--- main.js
+++ main.js
@@ -3,38 +3,29 @@ import createProduct from "./Product";
 
 const API_URL = "http://reto-bsale-backend.herokuapp.com/";
 
+// Request all categories
 async function getCategories() {
   let res = await fetch(API_URL + "categories");
   return res.json();
 }
 
-function toTitleCase(str) {
-  // Split string at every space
-  return str.replace(/\w\S*/g, (txt) => {
-    return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
-  });
-}
-
-let categoriesJSON = await getCategories();
-let categories = categoriesJSON.categories.map((cat) => {
-  return {
-    id: cat.id,
-    name: toTitleCase(cat.name),
-  };
-});
-
+// Request all products
 async function getProducts() {
   let res = await fetch(API_URL + "products");
   return res.json();
 }
 
+let categoriesJSON = await getCategories();
+let categories = categoriesJSON.categories;
+
 const productsJSON = await getProducts();
 const products = productsJSON.products;
-console.log(products);
 
+// Put all categories inside the sidebar
 const categoryList = document.getElementById("category-list");
 categories.map((li) => categoryList.appendChild(createCategory(li)));
 
+// Place all products inside the product grid
 const productList = document.getElementById("products");
 products.map((p) => {
   productList.appendChild(createProduct(p));
